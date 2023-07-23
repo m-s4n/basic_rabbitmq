9b6f6672863286e9a95e251d9cabd129d227e5c5
Mustafa SEYMEN <mustafa.seymen@bil.omu.edu.tr>
2023-07-23 12:29:03 +0300

    initial commit

---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+// Queue Oluşturma - producer ile aynı
+channel.QueueDeclare(queue: "gs", exclusive: false, durable: true, autoDelete: false);
+
+// Mesaj Okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "gs", autoAck: true, consumer: consumer);
+consumer.Received += (sender, e) => {
+    // Mesajın işlendiği alan
+    string mesaj = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(mesaj);
+};
+
+Console.ReadLine();
+  
---
+++ RabbitPublisher/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System.Text;
+using RabbitMQ.Client;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+// Queue Oluşturma 
+channel.QueueDeclare(queue: "gs", exclusive: false, durable: true, autoDelete: false);
+
+// Mesaj Gönderme
+// RabbitMQ mesajları byte[] olarak kabul eder
+while (true)
+{
+    string mesaj = Console.ReadLine() ?? "mesaj girilmedi";
+    byte[] byteMesaj = Encoding.UTF8.GetBytes(mesaj);
+    channel.BasicPublish(exchange: "", routingKey: "gs", body: byteMesaj);
+}
+
+
+
+
