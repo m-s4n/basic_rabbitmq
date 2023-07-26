9b02b6f005db54018007f7acddd4c4d74c5045ba
Mustafa SEYMEN <mustafa.seymen@bil.omu.edu.tr>
2023-07-26 21:07:24 +0300

    fanout exchange orneklemesi

---
+++ Fanout.Exchange.Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+// exchange
+channel.ExchangeDeclare(exchange: "fanout-exchange-1", type: ExchangeType.Fanout, durable: true, autoDelete: false);
+
+// kuyruk oluşturulur
+Console.Write("Kuyruk adını giriniz : ");
+string queueName = Console.ReadLine() ?? "queue_name";
+channel.QueueDeclare(queue: queueName, durable: true, autoDelete: false, exclusive: false);
+
+// kuyruk exchange'e bind edilir.
+channel.QueueBind(queue: queueName, exchange: "fanout-exchange-1", routingKey: string.Empty);
+
+//mesajları tüketmek için consumer oluşturulur
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+consumer.Received += (sender, e) => 
+{
+    string mesaj = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(mesaj);
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+};
+
+Console.Read();
+
+
+
+
+
+
---
+++ Fanout.Exchange.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+
+
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+// exchange
+channel.ExchangeDeclare(exchange: "fanout-exchange-1", type: ExchangeType.Fanout, durable: true, autoDelete: false);
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+// mesaj yayınlanır
+while (true)
+{
+    string mesaj = Console.ReadLine() ?? "Mesaj Boş";
+    byte[] byteMesaj = Encoding.UTF8.GetBytes(mesaj);
+    channel.BasicPublish(exchange: "fanout-exchange-1", routingKey: string.Empty, body: byteMesaj, basicProperties: properties);
+}
+
+
+
+
