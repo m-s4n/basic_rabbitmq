ae63c117756309d5fa3dd29fb2e5b71f6bad94bf
Mustafa SEYMEN <mustafa.seymen@bil.omu.edu.tr>
2023-07-27 16:47:20 +0300

    header exchange örneklemesi

---
+++ Header.Exchange.Consumer/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+// exchange
+channel.ExchangeDeclare(exchange: "header_exchange_1", type: ExchangeType.Headers, durable: true, autoDelete: false);
+
+// kuyruk oluşturulur
+var queueName = channel.QueueDeclare(durable: true, exclusive: false, autoDelete: false).QueueName;
+
+// header bilgileri alınır
+Console.Write("Header id value gir:");
+string idValue = Console.ReadLine() ?? "value_1";
+Console.Write("Header ad value gir:");
+string nameValue = Console.ReadLine() ?? "value_2";
+Console.Write("x-match gir:");
+string xmatchTur = Console.ReadLine() ?? "any";
+
+// kuyruk exchange bind edilir
+channel.QueueBind(queue: queueName, exchange: "header_exchange_1",routingKey: string.Empty, new Dictionary<string, object>{
+    ["x-match"] = xmatchTur,
+    ["id"] = idValue,
+    ["name"] = nameValue
+});
+
+// consumer
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+// mesaj al
+consumer.Received += (sender, e) => 
+{
+    string mesaj = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine(mesaj);
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+};
+
+Console.Read();
+
+
+
+
---
+++ Header.Exchange.Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+// exchange 
+channel.ExchangeDeclare(exchange: "header_exchange_1", type: ExchangeType.Headers,durable: true, autoDelete: false);
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+while(true)
+{
+    byte[] byteMesaj = Encoding.UTF8.GetBytes(Console.ReadLine() ?? "Boş mesaj");
+    Console.Write("Header id value gir:");
+    string idValue = Console.ReadLine() ?? "value_1";
+    Console.Write("Header ad value gir:");
+    string nameValue = Console.ReadLine() ?? "value_2";
+    properties.Headers = new Dictionary<string, object> ()
+    {
+       ["id"] = idValue,
+       ["name"] = nameValue
+
+    };
+
+    channel.BasicPublish(exchange: "header_exchange_1", routingKey: string.Empty, basicProperties: properties, body: byteMesaj);
+}
+
+
+
+
