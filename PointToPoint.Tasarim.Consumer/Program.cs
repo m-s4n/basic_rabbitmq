7d075b93550f421ae37abc6937907dbabeee7102
Mustafa SEYMEN <mustafa.seymen@bil.omu.edu.tr>
2023-07-28 10:40:15 +0300

    Point to point mesaj tasarımı tamamlandı

---
+++ PointToPoint.Tasarim.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+//P2P
+// mesaj direkt olarak kuyruğa gönderileceği için direkt kuyruk oluşturulur.
+channel.QueueDeclare(queue: "p2p_queue_1", durable: true, exclusive: false, autoDelete: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "p2p_queue_1", autoAck: false, consumer: consumer);
+
+consumer.Received += (sender, e) => 
+{
+    string mesaj = Encoding.UTF8.GetString(e.Body.Span);
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+    Console.WriteLine(mesaj);
+};
+
+Console.Read();
---
+++ PointToPoint.Tasarim.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+//P2P
+// mesaj direkt olarak kuyruğa gönderileceği için direkt kuyruk oluşturulur.
+channel.QueueDeclare(queue: "p2p_queue_1", durable: true, exclusive: false, autoDelete: false);
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+while (true)
+{
+    Console.Write("Mesaj giriniz: ");
+    string mesaj = Console.ReadLine() ?? "Mesaj girilmedi";
+    byte[] byteMesaj = Encoding.UTF8.GetBytes(mesaj);
+    channel.BasicPublish(exchange: string.Empty, routingKey: "p2p_queue_1", body: byteMesaj, basicProperties: properties);
+}
