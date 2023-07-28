554a07d6914734a6b6fad695ab00f5339bc78a10
Mustafa SEYMEN <mustafa.seymen@bil.omu.edu.tr>
2023-07-28 12:06:04 +0300

    work queue tasarımı tamamlandı

---
+++ WorkQueue.Tasarim.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+//Work queue
+// mesaj direkt olarak kuyruğa gönderileceği için direkt kuyruk oluşturulur.
+channel.QueueDeclare(queue: "work_queue_1", durable: true, exclusive: false, autoDelete: false);
+channel.BasicQos(prefetchCount: 1, prefetchSize: 0, global: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "work_queue_1", autoAck: false, consumer: consumer);
+
+consumer.Received += (sender, e) => 
+{
+    string mesaj = Encoding.UTF8.GetString(e.Body.Span);
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+    Console.WriteLine(mesaj);
+};
+
+Console.Read();
