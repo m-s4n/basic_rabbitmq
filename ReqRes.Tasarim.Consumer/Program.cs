bce6c39674e87f811678a9118f976ea0523fa0b9
Mustafa SEYMEN <mustafa.seymen@bil.omu.edu.tr>
2023-07-28 16:24:06 +0300

    request-response tasarim tamamlandi

---
+++ ReqRes.Tasarim.Consumer/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+#region  tasarım
+// response kuyrugu
+string receiverQueueName = "request_queue_1";
+channel.QueueDeclare(queue: receiverQueueName, durable: true, exclusive: false, autoDelete: false);
+
+// tüketici
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: receiverQueueName,autoAck: false, consumer: consumer );
+
+
+// mesaj okuma
+
+consumer.Received += (sender, e) => 
+{
+    string mesaj = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine($"Gelen mesaj: {mesaj}");
+    // mesaj işlenir
+
+    // Bu mesaj için response gönderilir
+    byte[] responseMesaj = Encoding.UTF8.GetBytes($"işlem tamamlandi : {mesaj}" );
+    IBasicProperties properties = channel.CreateBasicProperties();
+    properties.Persistent = true;
+    properties.CorrelationId = e.BasicProperties.CorrelationId; // response kuyruğunun adı
+    
+    // cevap gönder
+    channel.BasicPublish(exchange: string.Empty, routingKey: e.BasicProperties.ReplyTo, basicProperties: properties, body: responseMesaj);
+    // onay gönder
+    channel.BasicAck(deliveryTag:e.DeliveryTag, multiple: false);
+};
+
+Console.Read();
+
+#endregion
+
+
+
+
---
+++ ReqRes.Tasarim.Publisher/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+factory.UserName = "musti";
+factory.Password = "gopher";
+
+//Bağlantı Aktifleştirme
+using IConnection connection = factory.CreateConnection();
+
+// Kanal Açma
+using IModel channel = connection.CreateModel();
+
+#region  tasarım
+// request kuyrugu
+string requestQueueName = "request_queue_1";
+channel.QueueDeclare(queue: requestQueueName, durable: true, exclusive: false, autoDelete: false);
+
+// response kuyruğu
+string responseQueueName = "response_queue_1";
+channel.QueueDeclare(queue: responseQueueName, durable: true, exclusive: false, autoDelete: false);
+
+// correlation id
+string correlationId = Guid.NewGuid().ToString();
+
+#endregion
+
+#region request mesajını oluşturma ve gönderme
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+properties.CorrelationId = correlationId; // korelasyon id
+properties.ReplyTo = responseQueueName;  // yanıt mesajı hangi kuyruktan gelecek
+
+int i = 0;
+while (i < 5)
+{
+    i++;
+    Console.Write("Mesaj giriniz:");
+    byte[] byteMesaj = Encoding.UTF8.GetBytes(Console.ReadLine() ?? "mesaj girilmedi");
+    channel.BasicPublish(exchange: string.Empty, routingKey: requestQueueName, basicProperties: properties, body: byteMesaj);
+}
+#endregion
+
+#region response kuyruğunu dinleme
+// consomer oluşturma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: responseQueueName, autoAck: false, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    //korelasyonu uyan mesajlar işlenir
+    if(e.BasicProperties.CorrelationId == correlationId)
+    {
+        string mesaj = Encoding.UTF8.GetString(e.Body.Span);
+        Console.WriteLine($"Response : {mesaj}");
+        channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+    }
+    else
+    {
+        Console.WriteLine("mesaj başka yerden");
+        channel.BasicNack(deliveryTag: e.DeliveryTag, multiple: false, requeue: false);
+    }
+};
+
+Console.Read();
+
+#endregion
